654031fcbed30bb5f11d7979341d420a34e0fa86
Harol-d <harodgomez2007@gmail.com>
2026-05-05 14:20:23 -0500

    change: estrdcutura de carpeta para clonar el repo

---
+++ Inventario/Db/Db.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Inventario.Db
+{
+    public class Conexion
+    {
+        public Conexion() { }
+        public string connectionString =
+            "Server=localhost\\SQLEXPRESS;Database=Inventario;Trusted_Connection=True;";
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Conexión correcta a la base de datos");
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error de conexión a la base de datos " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Inventario/Services/ProveedorService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using Inventario.Model;
+using Inventario.Db;
+using System.Data.SqlClient;
+
+namespace Inventario.Services
+{
+    internal class ProveedorService
+    {
+        Conexion db = new Conexion();
+
+        public Boolean GuardarProveedor(Proveedor p)
+        {
+            string query = "INSERT INTO Proveedores (Nombre) VALUES (@nombre)";
+            using (SqlConnection conn = db.Conectar())
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@nombre", p.Nombre);
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+
+        public Boolean ActualizarProveedor(Proveedor p)
+        {
+            string query = "UPDATE Proveedores SET Nombre=@nombre WHERE IdProveedor=@id";
+            using (SqlConnection conn = db.Conectar())
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@id", p.Id);
+                cmd.Parameters.AddWithValue("@nombre", p.Nombre);
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+
+        public Boolean EliminarProveedor(int id)
+        {
+            string query = "DELETE FROM Proveedores WHERE IdProveedor=@id";
+            using (SqlConnection conn = db.Conectar())
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+
+    }
+}
+

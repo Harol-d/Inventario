826cbb80bd4c2136eff460fd311efea7691f6d80
harol-d <harodgomez2007@gmail.com>
2026-05-05 12:26:08 -0500

    Init: Con solo los archivos necesarios

---
+++ Model/Proveedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.Model
+{
+    public class Proveedor
+    {
+        public string Id { get; set; }
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+    }
+}
